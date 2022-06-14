0499a28fe333565f8c92f95353c9f4e3e3070246
DdooBuggy <yhiyo03@gmail.com>
2022-06-14 17:46:41 +0900

    Animated spring

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Animated, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -6,27 +7,28 @@ const Container = styled.View`
   justify-content: center;
   align-items: center;
 `;
-const Box = styled.TouchableOpacity`
+const Box = styled.View`
   background-color: tomato;
   width: 200px;
   height: 200px;
 `;
+const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  const [y, setY] = useState(0);
-  const [intervalId, setIntervalId] = useState(null);
+  const Y = new Animated.Value(0);
   const moveUp = () => {
-    const id = setInterval(() => setY((prev) => prev + 1), 10);
-    setIntervalId(id);
+    Animated.spring(Y, {
+      toValue: -200,
+      bounciness: 20,
+      useNativeDriver: true,
+    }).start();
   };
-  useEffect(() => {
-    if (y >= 200) {
-      clearInterval(intervalId);
-    }
-  }, [y, intervalId]);
+  Y.addListener(() => console.log(Y));
   return (
     <Container>
-      <Box onPress={moveUp} style={{ transform: [{ translateY: y }] }} />
+      <TouchableOpacity onPress={moveUp}>
+        <AnimatedBox style={{ transform: [{ translateY: Y }] }} />
+      </TouchableOpacity>
     </Container>
   );
 }
